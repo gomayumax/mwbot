212026986a94f3cae10e6e06f71b5923dad6b830
ごま <gomayumax@users.noreply.github.com>
2015-10-13 12:13:55 +0900

    当番　修正

--- scripts/clean_up.js
+++ scripts/clean_up.js
@@ -2,7 +2,13 @@ var CronJob = require('cron').CronJob;
 var people = [['小宮山','後藤','安井'],['池','北田','遠藤']];
 var souzi = ['フロア掃除機/棚','会議室ダスター/クイックル','会議室掃除機/コロコロ'];
 var weekDayJP = ['日','月','火','水','木','金','土'];
-
+var tobanList = [[0,1,2],[2,0,1],[1,2,0],[0,1,2],[2,0,1]];
+    //mon 1 0 1 2
+    //tue 2 2 0 1
+    //web 3 1 2 0
+    //thu 4 0 1 2
+    //fri 5 2 0 1
+ 
 module.exports = function(robot) {
   new CronJob('0 0 8 * * 1-5',function(){
       var nObj = new Date();
@@ -28,10 +34,7 @@ function addSouziForPeople(patternNum, day, patternNumDairi) {
   var msg = '';
   for(var num in people[patternNum] ){
     //掃除当番の添字作成
-    var souziNum = num+(day-1);
-    if( ((day+num) / 3) > (day - 1)  ){
-      souziNum = (num + (day - 1)) % 3;
-    }
+    var souziNum = tobanList[day-1][num];
     var toban = souzi[souziNum]; 
     //北田さんかつ金曜日だけ代理を立てる    
     if(day === 5 && people[patternNum][num] === '北田' ){
